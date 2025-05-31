f52df89b53029f52565fbd8b713fa8d2e9fff97b
SosoTlm <nitogx21@gmail.com>
2025-05-31 23:29:35 +0200

    Update auth.py

--- ProtectAPI/utils/auth.py
+++ ProtectAPI/utils/auth.py
@@ -1,6 +1,13 @@
+import json
 from fastapi import Header, HTTPException
-from config import API_KEY
+
+API_KEYS_FILE = "data/api_keys.json"
+
+def load_keys():
+    with open(API_KEYS_FILE, "r") as f:
+        return json.load(f)
 
 def verify_token(x_api_key: str = Header(...)):
-    if x_api_key != API_KEY:
+    valid_keys = load_keys()
+    if x_api_key not in valid_keys:
         raise HTTPException(status_code=401, detail="Invalid API Key")
